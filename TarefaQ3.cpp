d73b474add5bd1956b73b249ee1657367ae37a1c
Gustavo Alves Amorim <gustavo1679.sfa@gmail.com>
2026-04-03 22:45:54 -0300

    Implement user input and display for three values
    This program prompts the user to input three values and then displays them.

---
+++ TarefaQ3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double valor1, valor2, valor3;
+
+    cout << "Digite o primeiro valor: ";
+    cin >> valor1;
+
+    cout << "Digite o segundo valor: ";
+    cin >> valor2;
+
+    cout << "Digite o terceiro valor: ";
+    cin >> valor3;
+
+    cout << "\nValores digitados: \n";
+    cout << "Primeiro valor: " << valor1 << endl;
+    cout << "Segundo valor: " << valor2 << endl;
+    cout << "Terceiro valor: " << valor3 << endl;
+
+   
+
+    return 0;
+}
